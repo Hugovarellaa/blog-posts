f3f3b47f68e6a2f69111407a9031161f32e145f0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-24 15:30:36 -0300

    Interface Welcome

--- my-pokedex/src/pages/Welcome/index.tsx
+++ my-pokedex/src/pages/Welcome/index.tsx
@@ -1,10 +1,24 @@
-import { Content, Footer, WelcomeContainer } from "./styles";
+import LottieView from 'lottie-react-native';
+import { Content, Footer, SubTitle, Title, WelcomeContainer } from "./styles";
+
+import pokemonAnimation from '../../assets/pokemon.json';
 
 export function Welcome() {
   return (
     <WelcomeContainer>
-      <Content></Content>
-      <Footer></Footer>
+
+      <Content>
+        <LottieView
+          autoPlay
+          source={pokemonAnimation}
+          loop
+        />
+      </Content>
+
+      <Footer>
+        <Title>Bem vindo</Title>
+        <SubTitle>Encontre todos os Pokémons em um so lugar</SubTitle>
+      </Footer>
     </WelcomeContainer>
   )
 }
--- my-pokedex/src/pages/Welcome/styles.ts
+++ my-pokedex/src/pages/Welcome/styles.ts
@@ -9,7 +9,24 @@ export const Content = styled.View`
 `
 export const Footer = styled.View`
   height: 30%;
+
+  align-items: center;
+
   background-color: ${({ theme }) => theme.colors.backgroundCard.water};
   border-top-right-radius: 20px;
   border-top-left-radius: 20px;
+  
+`
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.background};
+  margin-top: 20px;
+
+`
+
+export const SubTitle = styled.Text`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.background};
+  margin-top: 20px;
 `
