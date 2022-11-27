427700092d1daa3632c78f244f5a1e402daeaf6d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-27 10:51:24 -0300

    Header dinamico pegando o type do pokemon

--- my-pokedex/src/pages/About/index.tsx
+++ my-pokedex/src/pages/About/index.tsx
@@ -1,7 +1,12 @@
+import { Feather } from '@expo/vector-icons';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import { Alert, Text } from "react-native";
+import { useTheme } from "styled-components";
 import { RootStackParamList } from "../../@types/navigation";
-import { AboutContainer } from "./styles";
+import { api } from "../../services/api";
+import { AboutContainer, BackButton, Header } from "./styles";
 
 
 type AboutScreenNavigationProp = NativeStackNavigationProp<
@@ -13,14 +18,98 @@ interface RouteParams {
   pokemonId: number
 }
 
+interface Stats {
+  base_stat: string;
+  stat: {
+    name: string
+    url: string
+  },
+}
+interface Abilities {
+  ability: {
+    name: string
+    url: string
+  },
+
+}
+
+export type TypeColors = 'grass' | 'fire' | 'water' | 'poison' | 'normal' | 'bug' | 'flying' | 'eletric' | 'ground'
+
+
+export interface PokemonTypeColors {
+  type: {
+    name: TypeColors
+  }
+}
+
+type PokemonProps = {
+  id: number;
+  name: string;
+  stats: Stats[]
+  abilities: Abilities[]
+  color: string
+  types: PokemonTypeColors[]
+}
+
 export function About() {
-  const navigation = useNavigation()
+  const [pokemon, setPokemon] = useState({} as PokemonProps)
+  const [loading, setLoading] = useState(true)
+
+  const { navigate } = useNavigation()
+  const { colors } = useTheme()
+
   const route = useRoute()
   const { pokemonId } = route.params as RouteParams
 
-  return (
-    <AboutContainer>
 
-    </AboutContainer>
+  async function getPokemonDetails() {
+    try {
+      const response = await api.get(`/pokemon/${pokemonId}`)
+      const { stats, abilities, id, name, types } = response.data
+
+      const currentType = types[0].type.name
+
+      const color = colors.backgroundCard[currentType] as TypeColors
+
+      setPokemon({
+        id,
+        name,
+        types,
+        color,
+        stats,
+        abilities,
+      })
+
+    } catch (error) {
+      Alert.alert("Opa!", 'Nao foi possível buscar o pokemon tente novamente')
+      console.log(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getPokemonDetails()
+  }, [])
+
+  return loading ? (
+    <Text style={{ marginTop: 200 }}>Carregando...</Text>
+  ) : (
+    (
+
+      <AboutContainer style={{ flex: 1 }}>
+        <Header type={pokemon.types[0].type.name}>
+          <BackButton>
+            <Feather name='chevron-left' size={24} color='#fff' />
+          </BackButton>
+
+          {/* <ContentImage>
+            <CircleImage source={circleSvg} />
+            <PokemonImage source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png` }} />
+          </ContentImage> */}
+
+        </Header>
+      </AboutContainer>
+    )
   )
 }
--- my-pokedex/src/pages/About/styles.ts
+++ my-pokedex/src/pages/About/styles.ts
@@ -1,3 +1,33 @@
+import { ScrollView, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
+import { TypeColors } from ".";
 
-export const AboutContainer = styled.View``
+interface TypeProps {
+  type: TypeColors
+}
+
+
+export const AboutContainer = styled(ScrollView)``
+
+export const Header = styled.View<TypeProps>`
+  background-color: ${({ type, theme }) => theme.colors.backgroundCard[type]};
+  height: 340px;
+  padding: 20px;
+  flex-direction: row;
+  align-items: center;
+
+  position: relative;
+`
+
+export const BackButton = styled(TouchableOpacity)`
+  position: absolute;
+  top: 70px;
+  left: 40px;
+
+`
+
+export const ContentImage = styled.View``
+
+export const CircleImage = styled.Image``
+
+export const PokemonImage = styled.Image``
